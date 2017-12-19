7c460bd0bb27a79881057998ceb1e038e698a79e
Jason K. Moore <moorepants@gmail.com>
2017-12-19 13:31:50 -0800

    Added initial layout of functions.

---
+++ safeskijump/functions.py
@@ -0,0 +1,47 @@
+parameters = {coeff_of_friction: 0.03,
+
+              }
+
+
+def compute_entry_speed(parent_slope_angle, start_pos, approach_len):
+
+    return entry_speed
+
+
+def generate_approach_curve(entry_speed, parent_slope_angle, start_pos,
+                            approach_len, max_acc):
+
+    return approach_x, approach_y
+
+
+def compute_design_speed():
+    """Returns the magnitude of the takeoff velocity out of the approach
+    curve."""
+
+    return design_speed
+
+
+def compute_flight_trajectory():
+
+    return
+
+
+def find_trajectory_point_where_path_is_parallel_to_parent_slope():
+
+    return
+
+
+def find_landing_transition_point():
+    # beginning of transition curve
+
+    return x, y
+
+
+def compute_landing_surface():
+
+    return x, y
+
+
+def calculate_landing_transition_curve():
+
+    return x, y
