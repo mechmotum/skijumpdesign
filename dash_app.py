1a864f28a534da067f4751d6d746d3139e9626cd
Jason K. Moore <moorepants@gmail.com>
2017-12-21 16:54:22 -0800

    Better organization and layout of the layout code.

--- dash_app.py
+++ dash_app.py
@@ -1,135 +1,87 @@
-import numpy as np
 import plotly.graph_objs as go
 import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 
-from safeskijump.functions import *
+from safeskijump.functions import make_jump, create_plot_arrays
 
-layout = go.Layout(autosize=False, width=800, height=800, yaxis={'scaleanchor': 'x'})
-fig = go.Figure(layout=layout)
+BS_URL = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'
 
 app = dash.Dash()
-app.css.append_css({'external_url': 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'})
+app.css.append_css({'external_url': BS_URL})
 
-app.layout = \
-    html.Div([
-        html.Div([
-            html.Div([
-                html.H1('Ski Jump Design'),
-                html.P('Start Position [m]'),
-                dcc.Input(
-                    id='start_pos',
-                    placeholder='Start Position [meters]',
-                    type='number',
-                    value='10'
-                ),
-                html.P('Approach Length [m]'),
-                dcc.Input(
-                    id='approach_len',
-                    placeholder='Approach Length [meters]',
-                    type='number',
-                    value='50'
-                ),
-                html.P('Slope Angle: 10 degrees', id='slope-text'),
-                dcc.Slider(
-                    id='slope_angle',
-                    min=0,
-                    max=45,
-                    step=1,
-                    value=10,
-                    marks={0: '0 [deg]', 45: '45 [deg]'},
-                    updatemode='drag'
-                ),
-                html.P('Takeoff Angle: 10 degrees', id='takeoff-text'),
-                dcc.Slider(
-                    id='takeoff_angle',
-                    min=0,
-                    max=45,
-                    step=1,
-                    value=20,
-                    marks={0: '0 [deg]', 45: '45 [deg]'},
-                    updatemode='drag'
-                ),
-                ],
-                className='col-md-4'),
-            html.Div([
-                dcc.Graph(id='my-graph', figure=fig)],
-                className='col-md-4'),
-        ], className='row'),
-    ], className='container')
+start_pos_widget = html.Div([
+    html.P('Start Position [m]'),
+    dcc.Input(id='start_pos',
+              placeholder='Start Position [meters]',
+              type='number',
+              value='10')
+    ])
 
+approach_len_widget = html.Div([
+    html.P('Approach Length [m]'),
+    dcc.Input(id='approach_len',
+              placeholder='Approach Length [meters]',
+              type='number',
+              value='50')
+    ])
 
-def make_jump(slope_angle, start_pos, approach_len, takeoff_angle):
+slope_angle_widget = html.Div([
+    html.P('Slope Angle: 10 degrees', id='slope-text'),
+    dcc.Slider(
+        id='slope_angle',
+        min=0,
+        max=45,
+        step=1,
+        value=10,
+        marks={0: '0 [deg]', 45: '45 [deg]'},
+        updatemode='drag')
+    ])
 
-    tolerable_acc = 1.5  # G
+takeoff_angle_widget = html.Div([
+    html.P('Takeoff Angle: 10 degrees', id='takeoff-text'),
+    dcc.Slider(
+        id='takeoff_angle',
+        min=0,
+        max=45,
+        step=1,
+        value=20,
+        marks={0: '0 [deg]', 45: '45 [deg]'},
+        updatemode='drag')
+    ])
 
-    takeoff_entry_speed = compute_approach_exit_speed(
-        slope_angle, start_pos, approach_len)
+controls_widget = html.Div([start_pos_widget, approach_len_widget,
+                            slope_angle_widget, takeoff_angle_widget],
+                           className='col-md-4')
 
-    curve_x, curve_y, _, _ = generate_takeoff_curve(
-        slope_angle, takeoff_entry_speed, takeoff_angle, tolerable_acc)
+layout = go.Layout(autosize=False,
+                   width=800,
+                   height=800,
+                   yaxis={'scaleanchor': 'x'})  # equal aspect ratio
 
-    ramp_entry_speed = compute_design_speed(
-        takeoff_entry_speed, slope_angle, curve_x, curve_y)
+graph_widget = html.Div([dcc.Graph(id='my-graph',
+                                   figure=go.Figure(layout=layout))],
+                        className='col-md-4')
 
-    curve_x, curve_y = add_takeoff_ramp(
-        takeoff_angle, ramp_entry_speed, curve_x, curve_y)
-
-    design_speed = compute_design_speed(
-        takeoff_entry_speed, slope_angle, curve_x, curve_y)
-
-    init_x = (start_pos + approach_len) * np.cos(np.deg2rad(slope_angle)) + curve_x[-1]
-    init_y = -(start_pos + approach_len) * np.sin(np.deg2rad(slope_angle)) + curve_y[-1]
-
-    traj_x, traj_y, vel_x, vel_y = compute_flight_trajectory(
-        slope_angle, (init_x, init_y), takeoff_angle, design_speed)
-
-    return curve_x, curve_y, traj_x, traj_y
-
-
-def create_plot_arrays(slope_angle, start_pos, approach_len, takeoff_angle,
-                       takeoff_curve_x, takeoff_curve_y, flight_traj_x,
-                       flight_traj_y):
-
-    # plot approach
-    l = np.linspace(start_pos, start_pos + approach_len)
-    x = l * np.cos(np.deg2rad(slope_angle))
-    y = -l * np.sin(np.deg2rad(slope_angle))
-    approach_xy = (x, y)
-
-    # plot takeoff curve
-    shifted_takeoff_curve_x = takeoff_curve_x + x[-1]
-    shifted_takeoff_curve_y = takeoff_curve_y + y[-1]
-    takeoff_xy = (shifted_takeoff_curve_x, shifted_takeoff_curve_y)
-
-    # plot takeoff angle line
-    takeoff_line_slope = np.tan(np.deg2rad(takeoff_angle))
-    takeoff_line_intercept = (shifted_takeoff_curve_y[-1] - takeoff_line_slope *
-                              shifted_takeoff_curve_x[-1])
-
-    x_takeoff = np.linspace(shifted_takeoff_curve_x[0],
-                            shifted_takeoff_curve_x[-1] + 5.0)
-    y_takeoff = takeoff_line_slope * x_takeoff + takeoff_line_intercept
-
-    # plot flight trajectory
-    flight_xy = (flight_traj_x, flight_traj_y)
-
-    return approach_xy, takeoff_xy, flight_xy
-
+app.layout = html.Div([html.H1('Ski Jump Design'),
+                       html.Div([controls_widget, graph_widget],
+                                className='row')],
+                      className='container')
 
 inputs = [Input('slope_angle', 'value'), Input('start_pos', 'value'),
           Input('approach_len', 'value'), Input('takeoff_angle', 'value')]
 
 
-@app.callback(Output('slope-text', 'children'), [Input('slope_angle', 'value')])
+@app.callback(Output('slope-text', 'children'),
+              [Input('slope_angle', 'value')])
 def update_slope_text(slope_angle):
     slope_angle = float(slope_angle)
     return 'Slope Angle: {:0.0f} degrees'.format(slope_angle)
 
 
-@app.callback(Output('takeoff-text', 'children'), [Input('takeoff_angle', 'value')])
+@app.callback(Output('takeoff-text', 'children'),
+              [Input('takeoff_angle', 'value')])
 def update_takeoff_text(takeoff_angle):
     takeoff_angle = float(takeoff_angle)
     return 'Takeoff Angle: {:0.0f} degrees'.format(takeoff_angle)
