fc7e19a3d7a1a80a56fc2ed259c676f12512e5ac
Jason K. Moore <moorepants@gmail.com>
2018-04-10 22:07:04 -0700

    Added jumps that have issues computing.

--- safeskijump/tests/test_functions.py
+++ safeskijump/tests/test_functions.py
@@ -13,7 +13,31 @@ def test_problematic_jump_parameters():
     # Invalid value in sqrt
     #make_jump(-10.0, 0.0, 30.0, 23.0, 0.2)
     #make_jump(-10.0, 0.0, 30.0, 20.0, 0.1)
-    #make_jump(-25.0, 0.0, 30.0, 20.0, 0.0)
+    #make_jump(-25.0, 0.0, 30.0, 20.0, 0.0)  # shouldn't be able to pass in 0.0
+
+    # Divide by zero in scipy/integrate/_ivp/rk.py
+    # RuntimeWarning: divide by zero encountered in double_scalars
+    #make_jump(-10.0, 10.0, 30.0, 20.0, 0.2)
+    #make_jump(-45.0, 0.0, 30.0, 0.0, 0.2)
+
+    #while loop ran more than 1000 times
+    # this first situation seems to be that the acceleration at max landing
+    # can't be below the threshold, thus the landing transition point ends up
+    # under the parent slope surface.
+    #make_jump(-45.0, 0.0, 30.0, 0.0, 0.2)
+    #make_jump(-45.0, 0.0, 30.0, 0.0, 0.3)
+    #make_jump(-10.0, 0.0, 30.0, 20.0, 2.0)
+    #make_jump(-15.0, 0.0, 30.0, 20.0, 3.0)
+    #make_jump(-15.0, 0.0, 30.0, 20.0, 2.7)
+
+    # ValueError: x and y arrays must have at least 2 entries
+    #make_jump(-10.0, 0.0, 30.0, 20.0, 2.0)
+    #make_jump(-10.0, 0.0, 30.0, 20.0, 1.5)
+    #make_jump(-15.0, 0.0, 30.0, 20.0, 3.0)
+    #make_jump(-15.0, 0.0, 30.0, 20.0, 2.7)
+
+    #ValueError: need at least one array to concatenate
+    #make_jump(-15.0, 0.0, 30.0, 20.0, 2.8)
 
     # Fall height too large
     with pytest.raises(InvalidJumpError):
