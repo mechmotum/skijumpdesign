9eaf28972e6e1dee3f6e2ec702051ac5e58f39a3
Jason K. Moore <moorepants@gmail.com>
2018-04-14 16:20:46 -0700

    Added draft setup.py and version tracking.

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+exec(open('safeskijump/version.py').read())
+
+setup(
+    name='safeskjump',
+    version=__version__,
+    author='Jason K. Moore',
+    author_email='moorepants@gmail.com',
+    url="https://gitlab.com/moorepants/safeskijump/",
+    description='Equivalent Fall Height Ski Jump Design Tool',
+    long_description=open('README.rst').read(),
+    keywords="engineering sports physics",
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['numpy',
+                      'scipy>=1.0',
+                      'matplotlib',
+                      'sympy',
+                      'cython',
+                      'fastcache',
+                      ],
+    extras_require={'app': ['plotly', 'dash', 'dash-renderer',
+                            'dash-html-components', 'dash-core-components'],
+                    'dev': ['pytest', 'sphinx']},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Physics',
+        ],
+)
