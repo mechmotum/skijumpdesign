7046006175ab0c83a76c392f3d707c34c8e597d5
Jason K. Moore <moorepants@gmail.com>
2018-04-19 14:16:12 -0700

    Added interpolation tests for Trajectory.

--- safeskijump/tests/test_trajectories.py
+++ safeskijump/tests/test_trajectories.py
@@ -1,7 +1,10 @@
+from math import isclose
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 from ..classes import Surface, Skier
+from ..trajectories import Trajectory
 
 
 def test_trajectory():
@@ -16,3 +19,43 @@ def test_trajectory():
 
     traj.plot_time_series()
     plt.show()
+
+
+def test_interp():
+
+    t = np.linspace(4.0, 16.0, num=1000)
+    x = np.cos(t)
+    y = np.sin(t)
+    vx = -np.sin(t)
+    vy = np.cos(t)
+    ax = -np.cos(t)
+    ay = -np.sin(t)
+
+    traj = Trajectory(t, np.vstack((x, y)).T,
+                      vel=np.vstack((vx, vy)).T,
+                      acc=np.vstack((ax, ay)).T)
+
+    assert isclose(traj.duration, 12.0)
+
+    assert np.allclose(traj.interp_pos_wrt_t(5.7),
+                       [np.cos(5.7), np.sin(5.7)], rtol=1e-4)
+    assert np.allclose(traj.interp_vel_wrt_t(5.7),
+                       [-np.sin(5.7), np.cos(5.7)], rtol=1e-4)
+    assert np.allclose(traj.interp_acc_wrt_t(5.7),
+                       [-np.cos(5.7), -np.sin(5.7)], rtol=1e-4)
+
+
+def test_interp_wrt_x():
+    t = np.linspace(5.0, 15.0)
+    x = np.linspace(0.0, 10.0)
+    y = 5.0 * x + 2.0
+
+    traj = Trajectory(t, np.vstack((x, y)).T)
+
+    assert isclose(traj.duration, 10.0)
+
+    res = traj.interp_wrt_x(2.33)
+
+    assert isclose(res[1], 2.33)  # x
+    assert isclose(res[2], 5.0 * 2.33 + 2.0)  # y
+    assert isclose(res[9], 5.0)  # slope
