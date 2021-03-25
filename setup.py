a7aaa37f11e2ee682204e701fe9fa3484629edce
Jason K. Moore <moorepants@gmail.com>
2021-03-25 15:50:21 +0100

    Updated setup.py for release.

--- setup.py
+++ setup.py
@@ -10,12 +10,13 @@ exec(open(os.path.join(this_dir, 'skijumpdesign', 'version.py')).read())
 setup(
     name='skijumpdesign',
     version=__version__,
-    author='Jason K. Moore, Mont Hubbard',
+    author='Jason K. Moore, Bryn Cloud, Mont Hubbard',
     author_email='moorepants@gmail.com',
     url="http://www.skijumpdesign.info",
-    description='Ski Jump Design Tool For Specified Equivalent Fall Height',
+    description=('Ski Jump Design and Analysis Tool For Specified Equivalent '
+                 'Fall Height'),
     long_description=open(os.path.join(this_dir, 'README.rst')).read(),
-    keywords="engineering sports physics design",
+    keywords="engineering sports physics design analysis",
     license='MIT',
     project_urls={
         'Web Application': 'http://www.skijumpdesign.info',
@@ -23,8 +24,7 @@ setup(
         'Source Code': 'https://gitlab.com/moorepants/skijumpdesign',
         'Issue Tracker': 'https://gitlab.com/moorepants/skijumpdesign/issues',
     },
-    python_requires='>=3.5',
-    py_modules=['skijumpdesignapp'],
+    python_requires='>=3.6',
     packages=find_packages(),
     include_package_data=True,  # includes things in MANIFEST.in
     zip_safe=False,
@@ -53,7 +53,7 @@ setup(
                             'coverage',
                             'pyinstrument']},
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: MIT License',
         'Intended Audience :: Science/Research',
         'Operating System :: OS Independent',
